03f32c54721775ec36acdfb7263c7c4c4e97c19f
Kevin Aguiar <kevin.aguiar@credija.com.br>
2018-11-28 10:43:15 -0200

    ADD: License to Package.json and Component CSS

--- src/components/cooltextarea/cooltextarea.vue
+++ src/components/cooltextarea/cooltextarea.vue
@@ -1,3 +1,3 @@
 <template src="./cooltextarea.html"></template>
-<style src="./cooltextarea.scss" lang="scss"></style>
+<style src="./cooltextarea.css" lang="css"></style>
 <script src="./cooltextarea.js"></script>
