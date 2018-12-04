cde695be12e3ffe5df4218d610123a02885beef4
Kevin Aguiar <kevinfaveri@hotmail.com>
2018-12-04 20:14:20 -0200

    FIX: Emoji picker not selectable for better emoji spamming

--- src/components/cooltextarea/cooltextarea.js
+++ src/components/cooltextarea/cooltextarea.js
@@ -4,7 +4,6 @@ import Popper from 'vue-popperjs';
 import 'vue-popperjs/dist/css/vue-popper.css';
 import { ObserveVisibility } from 'vue-observe-visibility';
 
-// TODO: Add Burguer index 
 export default {
   name: 'CoolTextArea',
   components: {
