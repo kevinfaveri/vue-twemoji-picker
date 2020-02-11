cc1f8dbf36c8b5d602e2258d1b4bc057e08aa51d
Kevin Aguiar <kevinfaveridev@gmail.com>
2020-02-11 00:14:44 -0300

    FIX: Reworked Wrapper

--- src/shims-vue-twemoji-picker.d.ts
+++ src/shims-vue-twemoji-picker.d.ts
@@ -2,7 +2,5 @@ declare module '@kevinfaguiar/vue-twemoji-picker' {
   import Vue from 'vue';
   class TwemojiPicker extends Vue {}
   class TwemojiTextarea extends Vue {}
-  const EmojiService: any;
-  const TextareaParser: any;
-  export { TwemojiTextarea, TwemojiPicker, EmojiService, TextareaParser };
+  export { TwemojiTextarea, TwemojiPicker };
 }
--- src/wrapper.ts
+++ src/wrapper.ts
@@ -1,10 +1,5 @@
 import TwemojiPickerComponent from './components/TwemojiPicker.vue';
 import TwemojiTextareaComponent from './components/TwemojiTextarea.vue';
-import EmojiServiceImport from './services/EmojiService';
-import TextareaParserImport from './services/TextareaParser';
 
 export const TwemojiPicker = TwemojiPickerComponent;
 export const TwemojiTextarea = TwemojiTextareaComponent;
-
-export const EmojiService = EmojiServiceImport;
-export const TextareaParser = TextareaParserImport;
